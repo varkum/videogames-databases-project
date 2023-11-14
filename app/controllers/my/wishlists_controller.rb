9960d862360867d592be77024d5ed110ee0ec773
Varun Kumar <varun.kumar@yale.edu>
2023-11-14 14:00:26 -0500

    show wishlist

--- app/controllers/my/wishlists_controller.rb
+++ app/controllers/my/wishlists_controller.rb
@@ -1,37 +1,23 @@
 class My::WishlistsController < ApplicationController
   before_action :set_wishlist
 
-
-  # GET /wishlists/1 or /wishlists/1.json
   def show
+    @wishlist_videogames = @wishlist.videogames
   end
 
- 
-
-  # GET /wishlists/1/edit
-  def edit
-  end
-
-
-  # PATCH/PUT /wishlists/1 or /wishlists/1.json
   def update
     videogame_to_add = Videogame.find(params[:videogame])
-    if Current.user.wishlist.videogames.include? videogame_to_add
-      Current.user.wishlist.videogames.destroy videogame_to_add
+    if @wishlist.videogames.include? videogame_to_add
+      @wishlist.videogames.destroy videogame_to_add
     else
-      Current.user.wishlist.videogames << videogame_to_add
+      @wishlist.videogames << videogame_to_add
    end
-
+   Rails.logger.info action_name + " here!"
    render partial: "videogames/videogame", object: videogame_to_add
   end
 
   private
     def set_wishlist
       @wishlist = Current.user.wishlist
-    end
-
-    # Only allow a list of trusted parameters through.
-    def wishlist_params
-      params.require(:wishlist).permit(:user_id)
     end
 end
